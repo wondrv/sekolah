5ae1eafb9e22ccf31cb443e4ee52e2d2753cc22a
wondrv <niiellpz@gmail.com>
2025-09-30 15:03:59 +0700

    chore: Update class documentation for TemplateRevision model

--- app/Models/TemplateRevision.php
+++ app/Models/TemplateRevision.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class TemplateRevision
+ *
+ * Represents a snapshot (revision) of a user template state at a point in time.
+ * The JSON snapshot stores key template fields allowing restore operations.
+ *
+ * @property int $id
+ * @property int $user_template_id
+ * @property string $type          Revision action type (e.g., activate, publish_draft, pre_restore)
+ * @property array|null $snapshot  JSON snapshot of template fields
+ * @property string|null $note     Optional human readable note
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ *
+ * @property-read UserTemplate $userTemplate
+ */
 class TemplateRevision extends Model
 {
     use HasFactory;
